3695d20f18e1657d528934042388d95ae7de7f59
Jihad D. Waspada <dewey992@gmail.com>
2017-06-11 23:04:25 +0700

    FIX bug state.snappedCoordinates is undefined

--- src/module/Event/reducer.js
+++ src/module/Event/reducer.js
@@ -4,7 +4,7 @@ const initialState = injectLoadingStates({
   events: [],
   lastItem: null,
   totalData: null
-});
+})
 
 export default function event(state = initialState, action) {
   switch (action.type) {
@@ -26,7 +26,7 @@ export default function event(state = initialState, action) {
         return Object.assign({}, errorLoading(state))
       }
 
-      return state;
+      return state
     }
 
     case 'GET_DATA_COUNT': {
@@ -36,7 +36,7 @@ export default function event(state = initialState, action) {
 
       return Object.assign({}, state, {
         totalData: action.payload.data
-      });
+      })
     }
 
     case 'SAVE_EVENT_REQUEST': {
@@ -56,36 +56,11 @@ export default function event(state = initialState, action) {
         return Object.assign({}, errorLoading(state))
       }
 
-      return state;
-    }
-
-    case 'FILL_POLYLINES': {
-      return Object.assign({}, {polylines: action.payload.poly})
-
-    }
-
-    case 'FETCH_SNAP_TO_ROAD': {
-      return Object.assign({}, startLoading(state))
-    }
-
-    case 'FETCH_SNAP_TO_ROAD_SUCCESS': {
-      if (!action.payload) {
-        return state
-      }
-
-      return Object.assign({}, finishLoading(state), { snappedCoordinates: action.payload.snappedCoordinates, placeIdArray: action.payload.placeIdArray })
-    }
-
-    case 'FETCH_SNAP_TO_ROAD_ERROR': {
-      if (action.payload.hasError) {
-        return Object.assign({}, errorLoading(state))
-      }
-
-      return state;
+      return state
     }
 
     default: {
-      return state;
+      return state
     }
   }
 }
